c46d11c6c71cda59b6796c9cc92db4d0e5aaffcf
Collin O'Connor <cmo56@pitt.edu>
2021-02-09 16:27:02 -0500

    included scenario title

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ScenarioHeader from './ScenarioHeader.js';
 import ScenarioItem from './ScenarioItem.js';
+import ScenarioSubItem from './ScenarioSubItem.js';
 import CriticalCriteriaItem from './CriticalCriteriaItem.js';
 import './App.scss';
 
@@ -17,6 +18,7 @@ class ScenarioForm extends React.Component {
                 <CriticalCriteriaItem />
             ],
         };
+
         this.handleClick = this.handleClick.bind(this);
     };
 
@@ -29,6 +31,8 @@ class ScenarioForm extends React.Component {
             data.push( <ScenarioHeader /> );
         } else if(e.target.id === 'add-item-button'){
             data.push( <ScenarioItem /> );
+        } else if(e.target.id === 'add-sub-item-button'){
+            data.push( <ScenarioSubItem /> );
         } else {
             critData.push( <CriticalCriteriaItem /> );
         }
@@ -41,21 +45,30 @@ class ScenarioForm extends React.Component {
     render() {
         return (
             <form id='scenario-form' className='scenario-form'>
+                <label id='title'>Scenario Title: 
+                    <input type='text'></input>
+                </label>
+
                 <div className='form-content'>
                     <fieldset id='form-items'>
                         <legend>Scenario Items</legend>
                         {this.state.data}
                     </fieldset>
-                    <div>
+
+                    <div id='add-buttons'>
                         <button id='add-header-button' onClick={this.handleClick}>Add Header</button>
                         <button id='add-item-button' onClick={this.handleClick}>Add Item</button>
+                        <button id='add-sub-item-button' onClick={this.handleClick}>Add Sub-Item</button>
                     </div>
+
                     <fieldset id="crit-items">
                         <legend>Critical Criteria</legend>
                         {this.state.critData}
                     </fieldset>
+
                     <button id='add-crit-item' onClick={this.handleClick}>Add Critical Item</button>
                 </div>
+
                 <button type='submit' id='submit-next'>Next →</button>
             </form>
         );
