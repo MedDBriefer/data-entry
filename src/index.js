80d7b0f8e16ec2f1814e4368745bab63c49c5dca
Collin O'Connor <cmo56@pitt.edu>
2021-02-14 21:53:55 -0500

    handle interventions

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ScenarioInfo from './ScenarioInfo.js';
 import ScenarioItem from './ScenarioItem.js';
+import Intervention from './Intervention.js';
 import './App.scss';
 
 var traumaSteplist = require('./data/trauma-scenario.json');
@@ -11,16 +12,32 @@ class ScenarioForm extends React.Component {
         super(props);
         this.state = {
             data : null,
+            interventions : [],
+            interventionCount : 0,
         };
 
         this.handleUpdate = this.handleUpdate.bind(this);
+        this.addIntervention = this.addIntervention.bind(this);
     };
 
     handleUpdate(e) {
         // TODO 
     }
 
-    componentDidMount() {
+    addIntervention(e) {
+        e.preventDefault();
+        let interventionCopy = this.state.interventions.slice();
+        let intvntnCount = this.state.interventionCount;
+        intvntnCount++;
+
+        interventionCopy.push(<Intervention num={intvntnCount} />);
+        this.setState({
+            interventions: interventionCopy,
+            interventionCount: intvntnCount,
+        });
+    }
+
+    componentDidMount(e) {
         // Import scenario data
         let scenarioData = [];
         traumaSteplist.forEach((item) => {
@@ -42,6 +59,16 @@ class ScenarioForm extends React.Component {
                     <div className='steplist-wrapper' id='step-data'>
                         {this.state.data}
                     </div>
+                </fieldset>
+
+                <fieldset id='intervention-forms'>
+                    <legend>Interventions</legend>
+
+                    <div className='interventions-wrapper'>
+                        {this.state.interventions}
+                    </div>
+                    
+                    <button type='button' id='add-intervention' onClick={this.addIntervention}>Add Intervention</button>
                 </fieldset>
 
                 <button type='submit' id='submit-next'>Next →</button>
