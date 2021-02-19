4ba17902e1361f34addd5621aa017fd75b4795e8
Collin O'Connor <cmo56@pitt.edu>
2021-02-18 22:05:30 -0500

    split into header and item

--- src/ScenarioItem.js
+++ src/ScenarioItem.js
@@ -1,23 +1,49 @@
 const ScenarioItem = (props) => {
     return (
+        (props.item.type === 'heading') ? 
+            <ScenarioHeader item={props.item} /> :
+        
         <div className='step-item'>
             <p>'{props.item.label}':</p> 
 
-            <label>
-                Intervention Type:
-                <select id={`${props.item.id}-type`}>
-                    <option value='na'>NA</option>
-                    <option value='say'>Say</option>
-                    <option value='do'>Do</option>
-                </select>
-            </label>
-
-            <label>
-                Callout: 
-                <input type='text' id={`${props.item.id}-callout`} placeholder='optional'></input>
-            </label>
+            {
+            (props.item.type === 'assessment') ?
+                <AssessmentFindings item={props.item} /> :
+                <InterventionType item={props.item} />
+            }
         </div>
     );
+}
+
+const ScenarioHeader = (props) => {
+    return (
+        <div className='list-item header-item'>
+            {props.item.label}
+        </div>
+    );
+}
+
+const InterventionType = (props) => {
+    return (
+        <label>
+            Intervention Type:
+            <select id={`${props.item.id}-type`}>
+                <option value='na'>NA</option>
+                <option value='say'>Say</option>
+                <option value='do'>Do</option>
+            </select>
+        </label>
+    );
+}
+
+const AssessmentFindings = (props) => {
+    return (
+        <label>
+            Assessment Findings: 
+            <input type='text' id={`${props.item.id}-findings`} placeholder='blank if unremarkable'></input>
+        </label>
+    );
+    
 }
 
 export default ScenarioItem;
