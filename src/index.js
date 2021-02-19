ff4c8c6d6180e5ebe211ac6385debc25baaad132
Collin O'Connor <cmo56@pitt.edu>
2021-02-18 22:45:33 -0500

    initial form testing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ScenarioInfo from './ScenarioInfo.js';
 import ScenarioItem from './ScenarioItem.js';
-import Intervention from './Intervention.js';
 import './App.scss';
 
 var traumaSteplist = require('./data/trauma-scenario.json');
@@ -11,30 +10,140 @@ class ScenarioForm extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            data : null,
-            interventions : [],
-            interventionCount : 0,
+            data : [],
+            form : {
+                id: null,
+                scenarioType: null,
+                info: {
+                    instructorInformation: null,
+                    patientInformation: {
+                        moulage: null,
+                        position: null,
+                        actions: null,
+                    },
+                    dispatchInfo: null,
+                    sceneAssessment: null,
+                },
+                assessmentFindings: {
+                    "assess-scene-safety": null,
+                    "assess-injury-mechanism": null,
+                    "assess-num-patients": null,
+                    "request-addl-help": null,
+                    "verbalizes-patient-condition": null,
+                    "determines-loc": null,
+                    "determines-life-threats": null,
+                    "assess-airway": null,
+                    "assess-breathing": null,
+                    "assess-ventilation": null,
+                    "assess-pulse": null,
+                    "assess-skin": null,
+                    "assess-bleeding": null,
+                    "assess-mouth-nose-face": null,
+                    "assess-scape-ears": null,
+                    "assess-perrl": null,
+                    "assess-trachea": null,
+                    "assess-jugular": null,
+                    "assess-spine": null,
+                    "inspects-chest": null,
+                    "palpate-chest": null,
+                    "auscultate-chest": null,
+                    "assess-abdomen": null,
+                    "assess-pelvis": null,
+                    "assess-genitalia": null,
+                    "assess-left-leg": null,
+                    "assess-right-leg": null,
+                    "assess-left-arm": null,
+                    "assess-right-arm": null,
+                    "assess-posterior-thorax": null,
+                    "assess-lumbar-buttocks": null,
+                },
+                initialVitalSigns: {
+                    BP: null,
+                    P: null,
+                    R: null,
+                    Skin: null,
+                    Spo2: null,
+                    ETCO2: null,
+                    GCS: null,
+                    Glucose: null,
+                    Pain: null,
+                    Temp: null,
+                },
+                SAMPLE: {
+                    S: null,
+                    A: null,
+                    M: null,
+                    P: null,
+                    L: null,
+                    E: null,
+                },
+                reassessmentVitals: {
+                    BP: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    P: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    R: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    Skin: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    Spo2: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    ETCO2: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    GCS: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    Glucose: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    Pain: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    },
+                    Temp: {
+                    "goodVitals": null,
+                    "badVitals": null,
+                    }
+                },
+                interventions: {
+                    "stabilizes-spine": null,
+                    "manages-airway": null,
+                    "oxygen-therapy": null,
+                    "manage-breathing-injury": null,
+                    "control-bleeding": null,
+                    "control-shock": null,
+                    "transport-priority": null,
+                },
+            },
         };
 
         this.handleUpdate = this.handleUpdate.bind(this);
-        this.addIntervention = this.addIntervention.bind(this);
+        this.submitForm = this.submitForm.bind(this);
     };
 
     handleUpdate(e) {
         // TODO 
+        console.log(e);
     }
 
-    addIntervention(e) {
-        e.preventDefault();
-        let interventionCopy = this.state.interventions.slice();
-        let intvntnCount = this.state.interventionCount;
-        intvntnCount++;
-
-        interventionCopy.push(<Intervention num={intvntnCount} />);
-        this.setState({
-            interventions: interventionCopy,
-            interventionCount: intvntnCount,
-        });
+    submitForm(e) {
+        this.forceUpdate();
+        console.log(this.state.form);
+        // TODO: actually connect to the firebase database
     }
 
     componentDidMount(e) {
@@ -51,7 +160,7 @@ class ScenarioForm extends React.Component {
 
     render() {
         return (
-            <form id='scenario-form' className='scenario-form'>
+            <form id='scenario-form' className='scenario-form' onChange={this.handleUpdate} action=''>
                 <ScenarioInfo />
 
                 <fieldset id='scenario-steps'>
@@ -61,17 +170,7 @@ class ScenarioForm extends React.Component {
                     </div>
                 </fieldset>
 
-                <fieldset id='intervention-forms'>
-                    <legend>Interventions</legend>
-
-                    <div className='interventions-wrapper'>
-                        {this.state.interventions}
-                    </div>
-                    
-                    <button type='button' id='add-intervention' onClick={this.addIntervention}>Add Intervention</button>
-                </fieldset>
-
-                <button type='submit' id='submit-next'>Next →</button>
+                <button type='button' id='submit-next' onClick={this.submitForm}>Next →</button>
             </form>
         );
     }
